f4db6657ca3f85c99850b46708af7b4515b542ba
Faslala Assis <faslala@faslala.com>
2021-07-30 10:12:38 -0300

    updates

---
+++ src/dist/theme.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var theme = {
+  colors: {
+    primary: '#6200ee',
+    background: '#00000014',
+    text: '#000000B3',
+    success: '#135E01'
+  },
+  fonts: {
+    regular: 'Roboto, sans-serif'
+  }
+};
+var _default = theme;
+exports["default"] = _default;
